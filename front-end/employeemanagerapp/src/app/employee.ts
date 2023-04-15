44f233a2b987005d8cc0b2ef83ae0e492dc4291d
wilmercorrea97 <wilmercorrea97@gmail.com>
2023-04-15 18:57:28 -0400

    realizando pruebas al endpoint

--- front-end/employeemanagerapp/src/app/employee.ts
+++
@@ -1,9 +0,0 @@
-export interface Employee {
-    id: number;
-    name: String;
-    email: String;
-    jobTitle: String;
-    phone: String;
-    imageUrl: String;
-    employeeCode: String; 
-}
--- front-end/employeemanagerapp/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const enviroment = {
-    production : false,
-    apiBaseUrl: 'http://127.0.0.1:80'
-};
